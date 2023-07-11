ac3162088b93ce3adb6a377fab6af26c1921b42e
Laura Giebler <laura@Lauras-iMac.local>
2023-07-11 12:54:46 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+const port = 3000
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req,res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us12.api.mailchimp.com/3.0/lists/d3a18a35a8"
+
+    const options = {
+        method: "POST",
+        auth: "lgiebler:e40dc0a8fd419b756165466d36104397-us12"
+    }
+
+    const request = https.request(url, options, function(response) {
+
+        if(response.statusCode === 200) {
+            res.sendFile(__dirname + "/sucess.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+
+    //request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", function(req,res) {
+    res.redirect("/")
+})
+
+
+
+
+
+app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`));
+
